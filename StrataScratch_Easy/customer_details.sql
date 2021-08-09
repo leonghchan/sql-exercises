12fb87c17782a425ac6b2f6197939f5ada4bf78b
Leon G H Chan <leonghchan17@gmail.com>
2021-08-09 12:11:23 +0100

    remove easy

--- StrataScratch_Easy/customer_details.sql
+++
@@ -1,9 +0,0 @@
--- Find the details of each customer regardless of whether the customer made an order. 
--- Output the customer's first name, last name, and the city along with the order details.
--- Your output should be listing the customer's orders not necessarily listing the customers. 
--- This means that you may have duplicate rows in your results due to a customer ordering several 
--- of the same items. Sort records based on the customer's first name and the order details in 
--- ascending order.
-
-SELECT first_name, last_name, city FROM customers AS c
-ORDER BY first_name ASC;
--- StrataScratch_Easy/top_5_states_with_5_star_businesses.sql
+++
@@ -1,9 +0,0 @@
--- Find the top 5 states with the most 5 star businesses. Output the state name along with the 
--- number of 5-star businesses and order records by the number of 5-star businesses in descending 
--- order. In case there are two states with the same result, sort them in alphabetical order.
-
-SELECT state, COUNT(*) FROM yelp_business
-WHERE stars = 5
-GROUP BY state
-ORDER BY COUNT(*) DESC, state ASC
-LIMIT 5;
